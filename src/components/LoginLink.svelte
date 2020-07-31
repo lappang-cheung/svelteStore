0fbf32a62b1e0c86f804ec209a4537abefac28fe
Leo Cheung <lappangcheung@gmail.com>
2020-07-31 16:59:58 -0400

    front end

---
+++ src/components/LoginLink.svelte
@@ -0,0 +1,21 @@
+<script>
+    import user from '../stores/user'
+    import { link } from 'svelte-routing'
+    // Login
+    import globalStore from '../stores/globalStore'
+</script>
+
+{#if $user.jwt}
+    <a href="/" 
+        class="logout-btn" 
+        on:click={() => {
+            globalStore.toggleItem('sidebar', false)
+        }}
+    >logout</a>
+{:else}
+    <a href="/"  
+        on:click={() => {
+            globalStore.toggleItem('sidebar', false)
+        }}
+    >login</a>
+{/if}
